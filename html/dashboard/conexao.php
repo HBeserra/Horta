9460ed1478c2962a5184bccb4846dd53a4ad0a33
HBeserra <Herbert.beserra@gmail.com>
2017-09-14 20:56:05 -0300

    Revert "Revert "Revert "v.1.2.5"""
    This reverts commit 5c318e6f526ff82918c502f65686aa4536039f61.

--- html/dashboard/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "mysql.hostinger.com.br";
-	$usuario = "u294779963_horta";
-	$senha = "abc123";
-	$dbname = "u294779963_horta";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
